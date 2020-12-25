27da3d52f85b7f60f3605a0676c9ac12532c2066
syazwancode <syazwan@invokeisdata.com>
2020-12-25 16:20:30 +0800

    new version

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
-const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+const video = document.getElementById('webcam');
+const enableWebcamButton = document.getElementById('start-btn');
+const stopButton = document.getElementById('stop-btn');
 
-// Check if webcam access is supported.
 function getUserMediaSupported() {
     return !!(navigator.mediaDevices &&
         navigator.mediaDevices.getUserMedia);
 }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
-// define in the next step.
 if (getUserMediaSupported()) {
     enableWebcamButton.addEventListener('click', enableCam);
+    stopButton.addEventListener('click', disableCam);
+    console.log('hooray!')
 } else {
     console.warn('getUserMedia() is not supported by your browser');
 }
 
-// Enable the live webcam view and start classification.
+
 function enableCam(event) {
-    // Only continue if the COCO-SSD has finished loading.
+
     if (!model) {
         return;
     }
-
-    // Hide the button once clicked.
-    event.target.classList.add('removed');
 
     // getUsermedia parameters to force video but not audio.
     const constraints = {
         video: true
     };
 
-    // Activate the webcam stream.
+    // activate webcam
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
         video.srcObject = stream;
         video.addEventListener('loadeddata', predictWebcam);
     });
+}
+
+function disableCam(event) {
+
+    if (!model) {
+        return;
+    }
+
+    if (video.srcObject) {
+        video.srcObject.getTracks().forEach(function (track) {
+            track.stop();
+            console.log('STOP!')
+        });
+    }
 }
 
 var children = [];
@@ -57,9 +66,10 @@ function predictWebcam() {
             // If we are over 66% sure we are sure we classified it right, draw it!
             if (predictions[n].score > 0.5) {
                 const p = document.createElement('p');
-                p.innerText = predictions[n].class + ' - with '
-                    + Math.round(parseFloat(predictions[n].score) * 100)
-                    + '% confidence.';
+                p.innerText = predictions[n].class + ' '
+                    + (predictions[n].score).toFixed(2)
+                // + Math.round(parseFloat(predictions[n].score) * 100)
+                // + '% confidence.';
                 p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
                     + (predictions[n].bbox[1] - 10) + 'px; width: '
                     + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
@@ -83,16 +93,10 @@ function predictWebcam() {
     });
 }
 
-// Store the resulting model in the global scope of our app.
 var model = undefined;
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
+//load model
 cocoSsd.load().then(function (loadedModel) {
     model = loadedModel;
-    // Show demo section now model is ready to use.
-    demosSection.classList.remove('invisible');
+    console.log('Model loaded!')
 });
